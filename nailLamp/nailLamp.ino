601ee132ae1a4e28f94819d437fb6c800de06f46
GaelicGrime <will.angus.blaylock@gmail.com>
2022-06-22 15:12:02 -0600

    Updated nailLamp.ino.

--- nailLamp/nailLamp.ino
+++ nailLamp/nailLamp.ino
@@ -1,14 +1,27 @@
 
 
+integer BEAM_IN_PIN = 2;
+integer TIME_AMOUNT_PIN = A0;
+integer BEAM_OUT_PIN = 3;
+integer LED_CONTROL_PIN = 5;
 
 
-void setup() 
+/*
+    - All setup on screen.
+    - PWM/brightness.
+    - Stop on removal of hand from the unit.
+    - Timer options.
+    - Upgrade to bigger touch screen and STM32F103C8T6.
+*/
+
+void setup()
 {
   // put your setup code here, to run once:
 
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 
 }
