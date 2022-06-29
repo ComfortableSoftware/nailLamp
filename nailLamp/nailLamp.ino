bd761f6e9fb5f5fed6b1f4811531d3db479dc3f7
GaelicGrime <will.angus.blaylock@gmail.com>
2022-06-28 22:43:08 -0600

    Updated nailLamp.inoproject2_3EjOIrGT49.pdf .

--- nailLamp/nailLamp.ino
+++ nailLamp/nailLamp.ino
@@ -1,17 +1,42 @@
 
 
-integer BEAM_IN_PIN = 2;
-integer TIME_AMOUNT_PIN = A0;
-integer BEAM_OUT_PIN = 3;
-integer LED_CONTROL_PIN = 5;
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+
+// For an ESP32 
+#define TFT_CS 15
+#define TFT_DC 27
+#define TFT_MOSI 23
+#define TFT_CLK 18
+#define TFT_RST 4
+#define TFT_MISO 19
 
 
 /*
     - All setup on screen.
-    - PWM/brightness.
+    - PWM/brightness of UV/LED.
+    - PWM brightness of backlight on display.
     - Stop on removal of hand from the unit.
     - Timer options.
     - Upgrade to bigger touch screen and STM32F103C8T6.
+
+    right to left on the display side
+    T-IRQ
+    T-DO
+    T-DIN
+    T_CS
+    T_CLK
+    SDO/MISO
+    LED
+    SCK
+    SDI/MOSI
+    DC
+    RESET
+    CS
+    GND
+    VCC
+
 */
 
 void setup()
