6de62e2749cce054f9ba8e1e36f983f6947b770b
GaelicGrime <will.angus.blaylock@gmail.com>
2022-07-07 02:44:03 -0600

    Updated nailLamp.ino.

--- .junk/pinouts.md
+++ .junk/pinouts.md
@@ -7,18 +7,18 @@ screen | ESP32
 -------|----------
 VCC    | VCC
 GND    | GND
-D_CS   | GPIO5/10
-RST    | GPIO4//7
-DC/RS  | GPIO27/30
-D_MOSI | GPIO23/18
-D_CLK  | GPIO18/11
-LED    | GPIO17/9
-D_MISO | GPIO19/12
-T_CLK  | GPIO14/31
-T_CS   | GPIO15/4
-T_MISO | GPIO12/32
-T_MOSI | GPIO13/34
-T_IRQ  | GPIO35/25
+D_CS   | GPIO15  // 5
+RST    | GPIO4
+DC/RS  | GPIO27
+D_MOSI | GPIO13  // 23
+D_CLK  | GPIO14  // 18
+LED    | GPIO17
+D_MISO | GPIO12  // 19
+T_CLK  | GPIO18  // 14
+T_CS   | GPIO5  // 15
+T_MISO | GPIO19  // 12
+T_MOSI | GPIO23  // 13
+T_IRQ  | GPIO35
 
 
 
@@ -26,46 +26,46 @@ T_IRQ  | GPIO35/25
 
 ### Open Smart 3.2inch TFT LCD Shield
 
-Screen  | ESP32
---------|-------
-SD_SCK  | NC
-SD_DO   | NC
-SD_DI   | NC
-SD_SS   | NC
-LCD_D5  | D19/12
-NC      | NC
-LCD_D3  | D22/17
-LCD_D2  | D23/18
-NC      | NC
-LCD_D1  | D4/7
-LCD_D0  | D16/RX2/8
-..      | ..
-LCD_D7  | D17/TX2/9
-LCD_D6  | D18/11
-SD_CS   | NC
-LCD_D4  | D21/14
-NC      | NC
-NC      | NC
-NC      | NC
-NC      | NC
-NC      | NC
-NC      | NC
-LCD_CS  | D27/30
-LCD_RS  | D14
-LCD_WR  | D12/32
-LCD_RD  | D13/34
-NC      | NC
-GND     | GND
-GND     | GND
-5V      | 5V
-NC      | NC
-NC      | NC
-NC      | NC
-NC      | NC
-..      | ..
-..      | ..
-LCD_RST | D26/29
-3V3     | NC
+Screen    | ESP32
+----------|------
+LCD_D5    | 13
+NC        | NC 12
+LCD_D3    | 11
+LCD_D2    | 10
+LCD_D1    | 9
+LCD_D0    | 8
+..        | ..
+LCD_D7    | 7
+LCD_D6    | 6
+SD_CS     | NC 5
+LCD_D4    | 4
+NC        | NC 3
+NC        | NC 2
+NC        | NC 1
+NC        | NC 0
+..  |  ..
+NC        | NC A5
+NC        | NC A5
+LCD_CS    | A3
+LCD_RS/CD | A2
+LCD_WR    | A1
+LCD_RD    | A0
+NC        | NC
+GND       | GND
+GND       | GND
+5V        | 5V
+NC        | NC
+NC        | NC
+NC        | NC
+NC        | NC
+..        | ..
+..        | ..
+LCD_RST   | 4
+3V3       | NC
+_-_       | _-_
+CLK       | 14
+POCI/MISO | 12
+PICO/MOSI | 13
 
 
 
--- nailLamp/nailLamp.ino
+++ nailLamp/nailLamp.ino
@@ -1,35 +1,125 @@
 
 
-#include "SPI.h"
-#include "Adafruit_GFX.h"
-#include "Adafruit_ILI9341.h"
+#include <MicroView.h>
 
-// For an ESP32
-#define TFT_CS 15
-#define TFT_DC 27
-#define TFT_MOSI 23
-#define TFT_CLK 18
-#define TFT_RST 4
-#define TFT_MISO 19
+
+#define BEAMISBROKEN beamBroken == LOW
+#define BEAMISNOTBROKEN beamBroken == HIGH
+#define CHECKBEAM beamBroken = digitalRead(INPIN)
+#define INPIN 3
+#define ONTIME 90
+#define OUTPIN 2
+#define SECONDS int(millis() / 1000)
+#define TURNOFFLEDS digitalWrite(OUTPIN, LOW)
+#define TURNONLEDS digitalWrite(OUTPIN, HIGH)
+#define WAIT_A_SEC delay(1000)
+
+
+bool beamBroken = false;
+char digits[80];
+float timeAtExpiry;
+int upTime, downTime;
+bool serialEnabled = True
 
 
 /*
-    - All setup on screen.
-    - PWM/brightness of UV/LED.
-    - PWM brightness of backlight on display.
-    - Stop on removal of hand from the unit.
-    - Timer options.
-
+  - Eventually if a move from the microview is needed.
+  - All setup on screen.
+  - PWM/brightness of UV/LED.
+  - PWM brightness of backlight on display.
+  - Stop on removal of hand from the unit.
+  - Timer options.
 */
+
+
+void printMe(char thingToPrint)
+{
+  if (serialEnabled == true)
+  {
+    Serial.println(thingToPrint);
+  }
+}
+
+
+void displayTimers()
+{
+  uView.clear(PAGE);
+  uView.display();
+  uView.setCursor(0,0);
+  sprintf(digits, "%02d %02d", upTime, downTime);
+  upTime += 1;
+  downTime -= 1;
+  // printMe("Displaying time");
+  printMe(digits);
+  uView.println(digits);
+  uView.display();
+}
+
 
 void setup()
 {
-  // put your setup code here, to run once:
+  if (serialEnabled)
+  {
+    Serial.begin(115200);
+  }
 
+  uView.begin();		// begin of MicroView
+  uView.clear(ALL);	// erase hardware memory inside the OLED controller
+  uView.clear(PAGE);	// erase the memory buffer, when next uView.display() is called, the OLED will be cleared.
+  uView.setFontType(3);
+  uView.setCursor(0,0);
+  uView.display();	// display the content in the buffer memory, by default it is the MicroView logo
+  upTime = 0;
+  downTime = ONTIME;
+  displayTimers();
+  pinMode(OUTPIN, OUTPUT);
+  TURNOFFLEDS;
+  pinMode(INPIN, INPUT_PULLUP);
 }
 
+
+//*
 void loop()
 {
-  // put your main code here, to run repeatedly:
-
+  CHECKBEAM;
+  while (BEAMISNOTBROKEN)
+  {
+    CHECKBEAM;
+  }
+  printMe("Beam broken");
+  timeAtExpiry = SECONDS + ONTIME;
+  upTime = 0;
+  downTime = ONTIME;
+  TURNONLEDS;
+  while (
+      (SECONDS < timeAtExpiry) &&
+      (BEAMISBROKEN)
+  )
+  {
+    displayTimers();
+    WAIT_A_SEC;
+    CHECKBEAM;
+  }
+  TURNOFFLEDS;
+  upTime = 0;
+  downTime = ONTIME;
+  displayTimers();
+  while (BEAMISBROKEN)
+  {
+    CHECKBEAM;
+    WAIT_A_SEC;
+  }
 }
+// */
+
+/*
+void loop()
+{
+  printMe("Looping.");
+  while (1)
+  {
+    displayTimers();
+    WAIT_A_SEC;
+  }
+}
+// */
